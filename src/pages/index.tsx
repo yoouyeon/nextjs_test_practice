245af4a276fe1542c1b71e7ce3396ae845ea532b
yoouyeon <jyeon.yoon59@gmail.com>
2023-05-01 16:07:42 +0900

    [Chore] Create Next App

---
+++ src/pages/index.tsx
@@ -0,0 +1,35 @@
+import { useRouter } from "next/router";
+import { Button } from "@/components/atoms/Button";
+
+export default function Home() {
+  const router = useRouter();
+  // 로그인 여부에 따라 다른 버튼 노출
+  const id: number = 1;
+  const logoutHandler = () => {
+    console.log("logoutHandler");
+  };
+  return (
+    <div>
+      This is Test Sample Todo Home Page
+      <Button
+        variant="contained"
+        onClick={() => {
+          router.push("/login");
+        }}
+      >
+        Login
+      </Button>
+      <Button variant="contained" onClick={logoutHandler}>
+        Logout
+      </Button>
+      <Button
+        variant="contained"
+        onClick={() => {
+          router.push(`/todo/${id}`);
+        }}
+      >
+        Go To My Todo
+      </Button>
+    </div>
+  );
+}
