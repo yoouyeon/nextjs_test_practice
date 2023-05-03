0e31ac7aad2e09bb5de0b93dad83877122cf37a2
yoouyeon <jyeon.yoon59@gmail.com>
2023-05-04 05:15:37 +0900

    [Style] 자잘한 style 수정과 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
 import "@/styles/globals.scss";
 import type { AppProps } from "next/app";
 import { RecoilRoot } from "recoil";
+import { ThemeProvider } from "@mui/material";
+import theme from "@/styles/theme";
 
 if (process.env.NEXT_PUBLIC_API_MOCKING === "enabled") {
   import("../mocks");
@@ -8,8 +10,10 @@ if (process.env.NEXT_PUBLIC_API_MOCKING === "enabled") {
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <RecoilRoot>
-      <Component {...pageProps} />
-    </RecoilRoot>
+    <ThemeProvider theme={theme}>
+      <RecoilRoot>
+        <Component {...pageProps} />
+      </RecoilRoot>
+    </ThemeProvider>
   );
 }
---
+++ src/styles/theme.ts
@@ -0,0 +1,14 @@
+import { createTheme } from "@mui/material";
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#a4d0a4",
+    },
+    secondary: {
+      main: "#ffd4d4",
+    },
+  },
+});
+
+export default theme;
